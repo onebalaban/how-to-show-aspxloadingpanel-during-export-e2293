4268de21c2380188b065a11d112821660cda3f31
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-16 12:42:10 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<%@ Register Assembly="DevExpress.Web.v17.2, Version=17.2.4.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a"
+    Namespace="DevExpress.Web" TagPrefix="dx" %>
+
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>How to show ASPxLoadingPanel during export</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <dx:ASPxGridViewExporter ID="gridExporter" runat="server">
+            </dx:ASPxGridViewExporter>
+            <dx:ASPxGridView ID="grid" runat="server" EnableCallbackAnimation="true">
+            </dx:ASPxGridView>
+            <asp:ScriptManager ID="ScriptManager1" runat="server">
+            </asp:ScriptManager>
+            <script type="text/javascript">
+                var prm = Sys.WebForms.PageRequestManager.getInstance();
+                prm.add_initializeRequest(prm_InitializeRequest);
+                prm.add_endRequest(prm_EndRequest);
+                function prm_InitializeRequest(sender, args) {
+                    panel.Show();
+                }
+                function prm_EndRequest(sender, args) {
+                    panel.Hide();
+                    btn.DoClick();
+                }
+            </script>
+            <dx:ASPxButton ID="btn" runat="server" ClientInstanceName="btn" ClientVisible="false"
+                OnClick="btn_Click" />
+            <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+                <ContentTemplate>
+                    <dx:ASPxButton ID="bnExportPDF" runat="server" Text="export to PDF" OnClick="bnExport_Click">
+                    </dx:ASPxButton>
+                    <dx:ASPxButton ID="bnExportXLS" runat="server" Text="export to XLS" OnClick="bnExport_Click">
+                    </dx:ASPxButton>
+                    <dx:ASPxButton ID="bnExportRRF" runat="server" Text="export to RTF" OnClick="bnExport_Click">
+                    </dx:ASPxButton>
+                </ContentTemplate>
+            </asp:UpdatePanel>
+            <dx:ASPxLoadingPanel ID="panel" ClientInstanceName="panel" runat="server" Modal="true">
+            </dx:ASPxLoadingPanel>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Data;
+using System.Web;
+using System.IO;
+using DevExpress.Web;
+using DevExpress.XtraPrinting;
+using DevExpress.XtraPrintingLinks;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void Page_Init(object sender, EventArgs e) {
+        grid.AutoGenerateColumns = true;
+        grid.Width = 400;
+        grid.Settings.HorizontalScrollBarMode = ScrollBarMode.Auto;
+        grid.SettingsExport.EnableClientSideExportAPI = true;
+        grid.DataSource = GetDataTable();
+        grid.DataBind();
+    }
+    private DataTable GetDataTable() {
+        DataTable dt = new DataTable();
+        dt.Columns.Add("NumberID", System.Type.GetType("System.Int32"));
+        for(int i = 0; i < 10; i++) {
+            dt.Columns.Add("Number" + i, System.Type.GetType("System.Int32"));
+        }
+        dt.PrimaryKey = new DataColumn[] { dt.Columns["NumberID"] };
+        for(int i = 0; i < 1000; i++) {
+            dt.Rows.Add(i, i, i, i, i, i, i, i, i, i, i);
+        }
+        return dt;
+    }
+    protected void bnExport_Click(object sender, EventArgs e) {
+        ASPxButton btn = sender as ASPxButton;
+
+        PrintingSystem ps = new PrintingSystem();
+        PrintableComponentLink lnk = new PrintableComponentLink(ps);
+        lnk.Component = gridExporter;
+
+        CompositeLink compositeLink = new CompositeLink(ps);
+        compositeLink.Links.AddRange(new object[] { lnk });
+        compositeLink.CreateDocument();
+
+        MemoryStream stream = new MemoryStream();
+        string type = string.Empty;
+        switch(btn.ID) {
+            case "bnExportPDF":
+                compositeLink.PrintingSystem.ExportToPdf(stream);
+                type = "pdf";
+                break;
+            case "bnExportXLS":
+                compositeLink.PrintingSystem.ExportToXls(stream);
+                type = "xls";
+                break;
+            case "bnExportRRF":
+                compositeLink.PrintingSystem.ExportToRtf(stream);
+                type = "rtf";
+                break;
+        }
+        Session["ExportStreame"] = stream;
+        Session["type"] = type;
+    }
+    protected void btn_Click(object sender, EventArgs e) {
+        MemoryStream stream = Session["ExportStreame"] as MemoryStream;
+        string type = Session["type"].ToString();
+        WriteToResponse(grid.ID, true, type, stream);
+    }
+    protected void WriteToResponse(string fileName, bool saveAsFile, string fileFormat, MemoryStream stream) {
+        if(Page == null || Page.Response == null) return;
+        string disposition = saveAsFile ? "attachment" : "inline";
+        Page.Response.Clear();
+        Page.Response.Buffer = false;
+        Page.Response.AppendHeader("Content-Type", string.Format("application/{0}", fileFormat));
+        Page.Response.AppendHeader("Content-Transfer-Encoding", "binary");
+        Page.Response.AppendHeader("Content-Disposition", string.Format("{0}; filename={1}.{2}", disposition, HttpUtility.UrlEncode(fileName).Replace("+", "%20"), fileFormat));
+        Page.Response.BinaryWrite(stream.ToArray());
+        Page.Response.End();
+    }
+}
