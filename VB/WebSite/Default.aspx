20ad1a0f4fc04da9929506056fe5c632b4a9e418
DevExpressExampleBot <fakeEmail>
2018-09-06 19:45:27 +0300

    Source auto update [skip ci]

--- VB/WebSite/Default.aspx
+++ VB/WebSite/Default.aspx
@@ -1,57 +1,57 @@
 ﻿<%@ Page Language="vb" AutoEventWireup="true" CodeFile="Default.aspx.vb" Inherits="_Default" %>
 
 <%@ Register Assembly="DevExpress.Web.v13.1, Version=13.1.4.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a"
-	Namespace="DevExpress.Web.ASPxGridView" TagPrefix="dx" %>
+    Namespace="DevExpress.Web.ASPxGridView" TagPrefix="dx" %>
 <%@ Register Assembly="DevExpress.Web.v13.1, Version=13.1.4.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a"
-	Namespace="DevExpress.Web.ASPxEditors" TagPrefix="dx" %>
+    Namespace="DevExpress.Web.ASPxEditors" TagPrefix="dx" %>
 <%@ Register Assembly="DevExpress.Web.v13.1, Version=13.1.4.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a"
-	Namespace="DevExpress.Web.ASPxGridView.Export" TagPrefix="dx" %>
+    Namespace="DevExpress.Web.ASPxGridView.Export" TagPrefix="dx" %>
 <%@ Register Assembly="DevExpress.Web.v13.1, Version=13.1.4.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a"
-	Namespace="DevExpress.Web.ASPxLoadingPanel" TagPrefix="dx" %>
+    Namespace="DevExpress.Web.ASPxLoadingPanel" TagPrefix="dx" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-	<title>Untitled Page</title>
+    <title>Untitled Page</title>
 </head>
 <body>
-	<form id="form1" runat="server">
-	<div>
-		<dx:ASPxGridViewExporter ID="gridExporter" runat="server">
-		</dx:ASPxGridViewExporter>
-		<dx:ASPxGridView ID="grid" runat="server">
-		</dx:ASPxGridView>
-		<asp:ScriptManager ID="ScriptManager1" runat="server">
-		</asp:ScriptManager>
+    <form id="form1" runat="server">
+    <div>
+        <dx:ASPxGridViewExporter ID="gridExporter" runat="server">
+        </dx:ASPxGridViewExporter>
+        <dx:ASPxGridView ID="grid" runat="server">
+        </dx:ASPxGridView>
+        <asp:ScriptManager ID="ScriptManager1" runat="server">
+        </asp:ScriptManager>
 
-		<script type="text/javascript">
-			var prm = Sys.WebForms.PageRequestManager.getInstance();
-			prm.add_initializeRequest(prm_InitializeRequest);
-			prm.add_endRequest(prm_EndRequest);
+        <script type="text/javascript">
+            var prm = Sys.WebForms.PageRequestManager.getInstance();
+            prm.add_initializeRequest(prm_InitializeRequest);
+            prm.add_endRequest(prm_EndRequest);
 
-			function prm_InitializeRequest(sender, args) {
-				panel.Show();
-			}
-			function prm_EndRequest(sender, args) {
-				panel.Hide();
-				btn.DoClick();
-			}
-		</script>
+            function prm_InitializeRequest(sender, args) {
+                panel.Show();
+            }
+            function prm_EndRequest(sender, args) {
+                panel.Hide();
+                btn.DoClick();
+            }
+        </script>
 
-		<dx:ASPxButton ID="btn" runat="server" ClientInstanceName="btn" ClientVisible="false"
-			OnClick="btn_Click" />
-		<asp:UpdatePanel ID="UpdatePanel1" runat="server">
-			<ContentTemplate>
-				<dx:ASPxButton ID="bnExportPDF" runat="server" Text="export to PDF" OnClick="bnExport_Click">
-				</dx:ASPxButton>
-				<dx:ASPxButton ID="bnExportXLS" runat="server" Text="export to XLS" OnClick="bnExport_Click">
-				</dx:ASPxButton>
-				<dx:ASPxButton ID="bnExportRRF" runat="server" Text="export to RRF" OnClick="bnExport_Click">
-				</dx:ASPxButton>
-			</ContentTemplate>
-		</asp:UpdatePanel>
-		<dx:ASPxLoadingPanel ID="panel" ClientInstanceName="panel" runat="server" Modal="true">
-		</dx:ASPxLoadingPanel>
-	</div>
-	</form>
+        <dx:ASPxButton ID="btn" runat="server" ClientInstanceName="btn" ClientVisible="false"
+            OnClick="btn_Click" />
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+            <ContentTemplate>
+                <dx:ASPxButton ID="bnExportPDF" runat="server" Text="export to PDF" OnClick="bnExport_Click">
+                </dx:ASPxButton>
+                <dx:ASPxButton ID="bnExportXLS" runat="server" Text="export to XLS" OnClick="bnExport_Click">
+                </dx:ASPxButton>
+                <dx:ASPxButton ID="bnExportRRF" runat="server" Text="export to RRF" OnClick="bnExport_Click">
+                </dx:ASPxButton>
+            </ContentTemplate>
+        </asp:UpdatePanel>
+        <dx:ASPxLoadingPanel ID="panel" ClientInstanceName="panel" runat="server" Modal="true">
+        </dx:ASPxLoadingPanel>
+    </div>
+    </form>
 </body>
 </html>
--- VB/WebSite/Default.aspx.vb
+++ VB/WebSite/Default.aspx.vb
@@ -1,45 +1,45 @@
-Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports System.Data
 Imports System.Web
 Imports System.IO
 Imports DevExpress.Web.ASPxEditors
 Imports DevExpress.Web.ASPxGridView.Export.Helper
-Imports DevExpress.XtraPrinting
 Imports DevExpress.Web.ASPxClasses
+Imports DevExpress.XtraPrinting
 Imports DevExpress.XtraPrintingLinks
 
 Partial Public Class _Default
-	Inherits System.Web.UI.Page
-	Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
-		grid.AutoGenerateColumns = True
-		grid.Width = 400
-		grid.Settings.HorizontalScrollBarMode = ScrollBarMode.Auto
+    Inherits System.Web.UI.Page
 
-		grid.DataSource = GetDataTable()
-		grid.DataBind()
-	End Sub
+    Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
+        grid.AutoGenerateColumns = True
+        grid.Width = 400
+        grid.Settings.HorizontalScrollBarMode = ScrollBarMode.Auto
 
-	Private Function GetDataTable() As DataTable
+        grid.DataSource = GetDataTable()
+        grid.DataBind()
+    End Sub
 
-		Dim dt As New DataTable()
-		dt.Columns.Add("NumberID", System.Type.GetType("System.Int32"))
-		For i As Integer = 0 To 9
-			dt.Columns.Add("Number" & i, System.Type.GetType("System.Int32"))
-		Next i
-		dt.Columns.Add("Number", System.Type.GetType("System.Int32"))
-		dt.PrimaryKey = New DataColumn() { dt.Columns("NumberID") }
+    Private Function GetDataTable() As DataTable
+
+        Dim dt As New DataTable()
+        dt.Columns.Add("NumberID", System.Type.GetType("System.Int32"))
+        For i As Integer = 0 To 9
+            dt.Columns.Add("Number" & i, System.Type.GetType("System.Int32"))
+        Next i
+        dt.Columns.Add("Number", System.Type.GetType("System.Int32"))
+        dt.PrimaryKey = New DataColumn() { dt.Columns("NumberID") }
 
 
-		For i As Integer = 0 To 999
-			dt.Rows.Add(i, i, i, i, i, i, i, i, i, i)
-		Next i
+        For i As Integer = 0 To 999
+            dt.Rows.Add(i, i, i, i, i, i, i, i, i, i)
+        Next i
 
-		Return dt
-	End Function
+        Return dt
+    End Function
 
-	Protected Sub bnExport_Click(ByVal sender As Object, ByVal e As EventArgs)
-		Dim btn As ASPxButton = TryCast(sender, ASPxButton)
+    Protected Sub bnExport_Click(ByVal sender As Object, ByVal e As EventArgs)
+        Dim btn As ASPxButton = TryCast(sender, ASPxButton)
 
         Dim ps As New PrintingSystemBase()
         Dim lnk As New PrintableComponentLinkBase(ps)
@@ -47,45 +47,40 @@ Partial Public Class _Default
 
         Dim compositeLink As New CompositeLinkBase(ps)
         compositeLink.Links.AddRange(New Object() { lnk })
-		compositeLink.CreateDocument()
+        compositeLink.CreateDocument()
 
-		Dim stream As New MemoryStream()
-		Dim type As String = String.Empty
-		Select Case btn.ID
-			Case "bnExportPDF"
+        Dim stream As New MemoryStream()
+        Dim type As String = String.Empty
+        Select Case btn.ID
+            Case "bnExportPDF"
                 compositeLink.PrintingSystemBase.ExportToPdf(stream)
                 type = "pdf"
-			Case "bnExportXLS"
+            Case "bnExportXLS"
                 compositeLink.PrintingSystemBase.ExportToXls(stream)
                 type = "xls"
-			Case "bnExportRRF"
+            Case "bnExportRRF"
                 compositeLink.PrintingSystemBase.ExportToRtf(stream)
                 type = "rtf"
-		End Select
-		Session("ExportStreame") = stream
-		Session("type") = type
-	End Sub
-	Protected Sub btn_Click(ByVal sender As Object, ByVal e As EventArgs)
-		Dim stream As MemoryStream = TryCast(Session("ExportStreame"), MemoryStream)
-		Dim type As String = Session("type").ToString()
-		WriteToResponse(grid.ID, True, type, stream)
-	End Sub
-	Protected Sub WriteToResponse(ByVal fileName As String, ByVal saveAsFile As Boolean, ByVal fileFormat As String, ByVal stream As MemoryStream)
-		If Page Is Nothing OrElse Page.Response Is Nothing Then
-			Return
-		End If
-		Dim disposition As String
-		If saveAsFile Then
-			disposition = "attachment"
-		Else
-			disposition = "inline"
-		End If
-		Page.Response.Clear()
-		Page.Response.Buffer = False
-		Page.Response.AppendHeader("Content-Type", String.Format("application/{0}", fileFormat))
-		Page.Response.AppendHeader("Content-Transfer-Encoding", "binary")
-		Page.Response.AppendHeader("Content-Disposition", String.Format("{0}; filename={1}.{2}", disposition, HttpUtility.UrlEncode(fileName).Replace("+", "%20"), fileFormat))
-		Page.Response.BinaryWrite(stream.ToArray())
-		Page.Response.End()
-	End Sub
+        End Select
+        Session("ExportStreame") = stream
+        Session("type") = type
+    End Sub
+    Protected Sub btn_Click(ByVal sender As Object, ByVal e As EventArgs)
+        Dim stream As MemoryStream = TryCast(Session("ExportStreame"), MemoryStream)
+        Dim type As String = Session("type").ToString()
+        WriteToResponse(grid.ID, True, type, stream)
+    End Sub
+    Protected Sub WriteToResponse(ByVal fileName As String, ByVal saveAsFile As Boolean, ByVal fileFormat As String, ByVal stream As MemoryStream)
+        If Page Is Nothing OrElse Page.Response Is Nothing Then
+            Return
+        End If
+        Dim disposition As String = If(saveAsFile, "attachment", "inline")
+        Page.Response.Clear()
+        Page.Response.Buffer = False
+        Page.Response.AppendHeader("Content-Type", String.Format("application/{0}", fileFormat))
+        Page.Response.AppendHeader("Content-Transfer-Encoding", "binary")
+        Page.Response.AppendHeader("Content-Disposition", String.Format("{0}; filename={1}.{2}", disposition, HttpUtility.UrlEncode(fileName).Replace("+", "%20"), fileFormat))
+        Page.Response.BinaryWrite(stream.ToArray())
+        Page.Response.End()
+    End Sub
 End Class
